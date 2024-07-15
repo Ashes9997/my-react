74e4ac882b0a077437fab31ed7039cf31e7ad2f3
Ashes9997 <ayesha.sharif9997@gmail.com>
2024-07-15 12:39:17 -0400

    react was not imported and there was a type error in HandleSubmit function  .now it is resolved.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Person from "./Person";
 
-function App() {
+import { Fragment,useState } from "react";
+
+
+const App = () => {
+  const [todoList, setTodoList]  = useState([]);
+  const [todo, setTodo] = useState("");
+  // let todo = "";
+
+  const HandleSubmit = () => {
+    // console.log("HandleSubmit" , todo)
+    let newTodos = [...todoList];
+    newTodos.push(todo);
+    setTodoList(newTodos);
+    setTodo ("");
+    // {text : newTodos , markAsDone : false}
+
+  }
+  const HandleChange = (e) => { 
+    // console.log(e.target.value);
+    setTodo = e.target.value;
+
+  }
+  console.log("todoList" , todoList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" className="form-control my-2" placeholder="Add your Task" onChange={HandleChange}/>
+      <button onClick={HandleSubmit} type="button" className="btn btn-primary">To Do</button>
+      <button type="button" className="btn btn-danger">Delete</button>
+
+      {todoList.map( (item, i) => <p key={i}>{ item.text}</p>)}
+
+
     </div>
-  );
-}
+  ); 
+
+   
+  
+};
 
 export default App;
---
+++ src/Person.js
@@ -0,0 +1,12 @@
+const Person = () => {
+    return (
+        <div>
+            <h4>Name </h4>
+            <p> Age </p>
+
+        </div>
+    );
+
+};
+
+export default Person;
